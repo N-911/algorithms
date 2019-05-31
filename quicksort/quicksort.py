2499c961f35b28581b8fa3ca64a2769aac07b79c
N-911 <nikolayenkoserhiy@gmail.com>
2019-05-31 06:24:12 +0300

    first

---
+++ quicksort/quicksort.py
@@ -0,0 +1,14 @@
+def quicksort(array):
+  if len(array) < 2:
+    # base case, arrays with 0 or 1 element are already "sorted"
+    return array
+  else:
+    # recursive case
+    comparable = array[0]
+    # sub-array of all the elements less than the pivot
+    less = [i for i in array[1:] if i <= comparable]
+    # sub-array of all the elements greater than the pivot
+    greater = [i for i in array[1:] if i > comparable]
+    return quicksort(less) + [comparable] + quicksort(greater)
+
+print(quicksort([100, 15, 2, 3, 50, 18]))
