6606dcaa1949f4fea968a2089b425eecc23cdfba
N-911 <nikolayenkoserhiy@gmail.com>
2019-06-20 00:53:48 +0300

    diskrete_alroritm(bacpack).py

--- diskrete_alroritm(bacpack).py
+++ diskrete_alroritm(bacpack).py
@@ -1,25 +1,29 @@
 m = [3,4,4,6,7]             #weight of elements
 v = [4,2,4,5,5]             #costs  elements
-# k = 12
+k1 = 12
 N = len (m)             #количество прежметов
-M = max(m)                 #  масса максимальная
+M = 15                 #  масса максимальная
 
 # i - max  elements
 # k - max weight bacpack
 
-
-F = [[0] * (N+1) for i in range(M + 1)]
 def bacpack(m, v):
-    for i in range(1, N + 1):
-        for k in range(1, M +1 ):
+    N = len (m)                #количество прежметов
+    M = max(m)                 #  масса максимальная
+    F = [[0] * (N) for i in range(M)]
+    for i in range(0, N):
+        for k in range(0, M):
             if m[i] <= k:
                 F[k][i] = max(v[i]+F[k-m[i]][i-1], F[k][i-1])
             else:
                 F[k][i] = F[k][i-1]
-
-    return F[M][N]
+    for g in F:
+        print (g)
+    return F[M-1][N-1]
+#     return F[M][N]
+bacpack(m, v)
 
 
 print(bacpack(m, v))
-
+# print ([[0] * (4) for i in range(2)])
 # print(F)
